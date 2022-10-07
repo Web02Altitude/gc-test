3d4e7235ff08c11d6a4e0964cd5eafb3e6547940
Web02Altitude <83783959+Web02Altitude@users.noreply.github.com>
2022-10-07 15:11:16 -0400

    manque img responsive dans lightbox

--- resources/js/site.js
+++ resources/js/site.js
@@ -3,28 +3,30 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 
-// lightningbox
-// import lightningimg from 'lightningbox'
-// lightningimg('.gallery > a')
-
 // glightbox
 import GLightbox from 'glightbox';
-const lightbox = GLightbox({ 
-    selector: '.gallery_element',
-    loop: true,
-    // skin: 'mon-theme',
-    openEffect: 'zoom',
-    closeEffect: 'fade',
-    cssEfects: {
-      fade: { in: 'fadeIn', out: 'fadeOut' },
-      zoom: { in: 'zoomIn', out: 'zoomOut' }
-    }
- });
-
-// No context menu
-// const imgs = document.querySelectorAll('img')
-// imgs.forEach(img => {
-//     img.addEventListener('contextmenu', (e) => {
-//         e.preventDefault()
-//     })
+const lightbox = GLightbox({
+  selector: '.gallery_element',
+  loop: true,
+  // skin: 'mon-theme',
+  openEffect: 'zoom',
+  closeEffect: 'fade',
+  cssEfects: {
+    fade: { in: 'fadeIn', out: 'fadeOut' },
+    zoom: { in: 'zoomIn', out: 'zoomOut' }
+  }
+});
+// const gals = document.querySelectorAll('*[class^="galerie"]')
+// gals.forEach(gal => {
+//   const lightbox = GLightbox({
+//     selector: gal,
+//     loop: true,
+//     // skin: 'mon-theme',
+//     openEffect: 'zoom',
+//     closeEffect: 'fade',
+//     cssEfects: {
+//       fade: { in: 'fadeIn', out: 'fadeOut' },
+//       zoom: { in: 'zoomIn', out: 'zoomOut' }
+//     }
+//   });
 // })
